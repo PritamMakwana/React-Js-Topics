ee12d8a5636351dfdf94c89ec7f9b0fc60e9cb5c
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2022-11-28 18:19:01 +0530

    one component in use anthoer component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import User from './User.js';
 
 function App() {
+
+  function Apple(){
+    return(
+      <div>Apple Component</div>
+    )
+  }
+
+
   return (
     <div className="App">
       <h1>Hello world</h1>
       <User/>
       <User></User>
+      <Apple/>
+      {Apple()}
     </div>
   );
 }

5aa7b6d7d51464cfb73094cbd25641cdfda88a81
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2022-11-29 15:44:16 +0530

    basic form

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import Student from './Student';
 import User from './User';
 
 function App(){
- const [print,setPrint] = useState(false);
+ const [name,setName] = useState("");
+ const [tnc,setTnc] = useState(false);
+ const [interest,setInterest] = useState("");
+
+ function getFormData(e){
+  e.preventDefault();
+  console.log(name,tnc,interest);
+ }
 
   return(
     <div className='App'>
-      {
-        print?<h1>Hello</h1>:null
-      }
-
-      <button onClick={()=>setPrint(true)}>Show</button>
-
-      <button onClick={()=>setPrint(false)}>hide</button>
-
-      <button onClick={()=>setPrint(!print)}>Toggle </button>
+      <h1>Handle form in react</h1>
+      <form onSubmit={getFormData}>
+      <input type="text" placeholder='name' onChange={(e)=>setName(e.target.value)} />
+      <br/>
+      <select onChange={(e)=>setInterest(e.target.value)} >
+        <option>Marvel</option>
+        <option>DC</option>
+      </select>
+      <br/>
+      <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)} />
+      <span>Accept Terms</span>
+      <br/>
+      <button type="submit">submit</button>
+      </form>
 
 
     </div>

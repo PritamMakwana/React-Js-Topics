fff4e177d466322f254b0e171e792ed433f668a4
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2022-11-30 19:31:11 +0530

    constructor life cycle method

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import { Component, useState } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Profile from './Profile';
 import Student from './Student';
 import User from './User';
 
-function App(){
+// React Life cycle three phrash :
+// Loaded
+// Update
+// Removed
 
-  function getData(){
-    alert("Hello from app");
-  }  
+//constuctor life cycle method 
+
+class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      name : "admin"
+    }
+  }
+
+  render(){
   return(
     <div className='App'>
-       <h1>Pass function as props</h1>
-      <User data={getData}/>
-      <Student data={getData}/>
-      <Profile data={getData}/>
+       <h1>Name {this.state.name}</h1>
     </div>
   )
+  }
 }
 
 export default App;
